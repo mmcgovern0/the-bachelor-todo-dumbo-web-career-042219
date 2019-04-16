780d317afe29f68712b8d04d288b6679fc0fce03
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-16 14:16:04 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -17,7 +17,11 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |season, contestants|
+    contestants.each do |contestants_hash|
+      if contestants_hash["hometown"] == hometown
+        counter += 1
 end
 
 def get_occupation(data, hometown)
