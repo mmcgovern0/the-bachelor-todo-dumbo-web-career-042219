aec4f11b5be0c7cb898f959ad96ea6d923a3e3ed
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-16 14:18:05 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -29,7 +29,13 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |contestants_hash|
+      if contestants_hash["hometown"] == hometown
+        return contestants_hash["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
