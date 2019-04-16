2ed90fd6eb58183be7df1cbd90b2f2c3908e3197
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-16 14:13:12 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -10,7 +10,8 @@ def get_contestant_name(data, occupation)
   data.each do |season, contestants|
     contestants.each do |contestants_hash|
       if contestants_hash["occupation"] == occupation
-       return contestants["name"]
+       return contestants_hash["name"]
+     end
     end
   end
 end
