0afc72035a31c2e4464bb1c9653368b9aa40d7f3
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-16 14:22:17 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,49 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |contestants|
+    if contestants["status"] == "Winner"
+      return contestants["name"].split(" ").first
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |contestants_hash|
+      if contestants_hash["occupation"] == occupation
+       return contestants_hash["name"]
+     end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |season, contestants|
+    contestants.each do |contestants_hash|
+      if contestants_hash["hometown"] == hometown
+        counter += 1
+      end
+    end
+  end
+  counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |contestants_hash|
+      if contestants_hash["hometown"] == hometown
+        return contestants_hash["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  age_total = 0
+  num_of_contestants = 0
+  data[season].each do |contestants|
+    age_total += (contestants["age"]).to_i
+    num_of_contestants += 1
+  end
+  (age_total / num_of_contestants.to_f).round(0)
 end
