e110a48b27e5b84f5a4cc39670c5463f4a2974c1
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-16 14:05:58 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,7 +1,7 @@
 def get_first_name_of_season_winner(data, season)
   data[season].each do |contestants|
     if contestants["status"] == "winner"
-      contestants
+      contestants[]
 end
 
 def get_contestant_name(data, occupation)
